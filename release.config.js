8f35448bbf65b0ec20117bb4670f18ef3e1d421b
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-02-24 00:20:06 -0300

    fix: ajusta o publish para usar timestamps

--- release.config.js
+++ release.config.js
@@ -4,4 +4,17 @@ export default {
     { name: 'beta', channel: 'beta' }, // Versões beta para o branch beta
     { name: 'develop', prerelease: 'next' }, // Versões next para o branch develop
   ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        pkgRoot: '.', // Diretório raiz do pacote
+        tarballDir: 'dist', // Diretório onde o pacote será empacotado
+        tag: `next-${process.env.TIMESTAMP_VERSION || 'unknown'}`, // Inclui o timestamp na tag
+      },
+    ],
+  ],
 }
