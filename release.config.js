422abe4606b88d83ef5cb4f718943c8bbf8e27bc
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-02-24 01:49:29 -0300

    chore: tenta ajustar o publish next

--- release.config.js
+++ release.config.js
@@ -1,33 +1,25 @@
-const branch = process.env.GITHUB_REF_NAME || 'unknown'
-const tagFormat = branch === 'develop' ? 'v${version}-next.${process.env.TIMESTAMP}' : 'v${version}'
 export default {
   branches: [
-    { name: 'main', channel: 'latest' }, // Versões estáveis para o branch main
-    { name: 'beta', channel: 'beta' }, // Versões beta para o branch beta
+    { name: 'main', channel: 'latest' },
+    { name: 'beta', channel: 'beta' },
     {
       name: 'develop',
+      prerelease: 'next',
       channel: 'next',
     },
   ],
-  tagFormat,
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'angular',
-        releaseRules: [
-          { type: 'chore', release: 'patch' },
-          { type: 'build', release: 'patch' },
-        ],
-      },
-    ],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/npm',
       {
         npmPublish: true,
-        tag: 'next.${process.env.TIMESTAMP}',
+        tarballDir: 'dist',
+        tag: 'next-${process.env.TIMESTAMP}', // Inclui o timestamp na tag do npm
       },
     ],
+    '@semantic-release/github',
   ],
+  tagFormat: 'v${version}-next.${process.env.TIMESTAMP}', // Formato da tag no Git
 }
