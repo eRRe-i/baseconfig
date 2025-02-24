de8fd4fb6d153ece40869d32bfb9cd8ac30e0f57
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-02-24 10:54:00 -0300

    chore: retorna prerelease pra next

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ export default {
     { name: 'beta', channel: 'beta' }, // Versões beta para o branch beta
     {
       name: 'develop',
-      prerelease: true,
+      prerelease: 'next',
       channel: 'next',
     },
   ],
