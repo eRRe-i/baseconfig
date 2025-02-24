be1a1d07100008fab73e5da7b57e32c7a2e69083
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-02-24 01:13:26 -0300

    chore: ajusta o release para retirar o incrementador automático de develop

--- release.config.js
+++ release.config.js
@@ -1,3 +1,5 @@
+const branch = process.env.GITHUB_REF_NAME || 'unknown'
+const tagFormat = branch === 'develop' ? 'v${version}-next-${process.env.TIMESTAMP}' : 'v${version}'
 export default {
   branches: [
     { name: 'main', channel: 'latest' }, // Versões estáveis para o branch main
@@ -8,6 +10,7 @@ export default {
       channel: 'next',
     },
   ],
+  tagFormat,
   plugins: [
     [
       '@semantic-release/commit-analyzer',
@@ -24,6 +27,7 @@ export default {
       '@semantic-release/npm',
       {
         npmPublish: true,
+        tag: 'next-${process.env.TIMESTAMP}', // Inclui o timestamp na tag do npm
       },
     ],
   ],
