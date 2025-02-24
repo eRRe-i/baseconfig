e99101b9b06ad2a519f65364dd4b8bb533d35722
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-02-24 02:05:44 -0300

    chore: retornar ao publish padrão :(

--- release.config.js
+++ release.config.js
@@ -1,15 +1,13 @@
-const branch = process.env.GITHUB_REF_NAME || 'unknown'
-const tagFormat = branch === 'develop' ? 'v${version}-next.${process.env.TIMESTAMP}' : 'v${version}'
 export default {
   branches: [
     { name: 'main', channel: 'latest' }, // Versões estáveis para o branch main
     { name: 'beta', channel: 'beta' }, // Versões beta para o branch beta
     {
       name: 'develop',
+      prerelease: 'next',
       channel: 'next',
     },
   ],
-  tagFormat,
   plugins: [
     [
       '@semantic-release/commit-analyzer',
@@ -26,7 +24,6 @@ export default {
       '@semantic-release/npm',
       {
         npmPublish: true,
-        tag: 'next.${process.env.TIMESTAMP}',
       },
     ],
   ],
